36ca191b8fffa29ddf396dae1f7fa1c4a7d240cb
nhopvph57720 <phamvannho1212@gmail.com>
2025-11-07 20:02:22 +0700

    Revert "Feature/giao dien"

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,18 +3,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabNavigator from "./BottomTabNavigator";
 import OrderScreen from "../screens/OrderScreen";
 import OrderDetail from "../screens/OrderDetail";
-import LoginScreen from "../screens/LoginScreen"; // ✅ Thêm dòng này
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen" // ✅ Màn hình khởi đầu
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Main" component={BottomTabNavigator} />
         <Stack.Screen name="OrderScreen" component={OrderScreen} />
         <Stack.Screen name="OrderDetail" component={OrderDetail} />
